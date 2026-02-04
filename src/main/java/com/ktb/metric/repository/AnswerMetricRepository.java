364ca7e37468f28f70642870aa71c7ae2b4b0004
mdgome <mdgome3@gmail.com>
2026-02-04 21:04:24 +0900

    fix:[#154] 누락된 Repository Class 추가
    (cherry picked from commit 48b341bb1e596434f4c9b21af613ac5d83232141)
    (cherry picked from commit 66966f428d113dbe85cabd4ab2b1216fbc02b7ef)

---
+++ src/main/java/com/ktb/metric/repository/AnswerMetricRepository.java
@@ -0,0 +1,20 @@
+package com.ktb.metric.repository;
+
+import com.ktb.metric.domain.AnswerMetric;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnswerMetricRepository extends JpaRepository<AnswerMetric, Long> {
+
+    @Query("""
+            SELECT am FROM AnswerMetric am
+            JOIN FETCH am.metric m
+            WHERE am.answer.id = :answerId
+            ORDER BY m.id ASC
+            """)
+    List<AnswerMetric> findByAnswerIdWithMetric(@Param("answerId") Long answerId);
+}
