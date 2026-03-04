2b7d0bb014be46335691c9386671b89336327d63
yeongmin <hwongyoungmin@gmail.com>
2026-03-04 16:52:51 +0900

    refactor(file): Presigned URL 예외를 ErrorCode 기반으로 일원화

--- src/main/java/com/ktb/file/exception/FileInvalidMetadataException.java
+++ src/main/java/com/ktb/file/exception/FileInvalidMetadataException.java
@@ -5,12 +5,16 @@ import com.ktb.common.exception.BusinessException;
 
 public class FileInvalidMetadataException extends BusinessException {
 
+    public FileInvalidMetadataException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public FileInvalidMetadataException(ErrorCode errorCode, String reason) {
+        super(errorCode, String.format("%s %s", errorCode.getMessage(), reason));
+    }
+
     public FileInvalidMetadataException(String reason) {
         super(ErrorCode.FILE_INVALID_METADATA,
-            String.format(
-                "%s %s"
-                , ErrorCode.FILE_INVALID_METADATA.getMessage()
-                , reason
-            ));
+                String.format("%s %s", ErrorCode.FILE_INVALID_METADATA.getMessage(), reason));
     }
 }
