548762c1bfb3e728a8c6590b9b408c2f0cf082be
mdgome <mdgome3@gmail.com>
2026-03-30 12:13:34 +0900

    feat:[#253] Hikari DB pool 적용

---
+++ src/main/java/com/ktb/common/config/DbPoolConfig.java
@@ -0,0 +1,24 @@
+package com.ktb.common.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class DbPoolConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.hikari")
+    public HikariDataSource dataSource(DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+    }
+}
